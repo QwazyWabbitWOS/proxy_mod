bc6810458c930ea1d32c54396b7aaa3fe9014c9b
QwazyWabbit <gandalf.2004@gmail.com>
2020-10-26 19:29:57 -0700

    Reformat. Spaces to tabs, etc

--- pm_main.c
+++ pm_main.c
@@ -7,157 +7,159 @@
 HMODULE hGameDLL;
 
 game_import_t	pmi; // original import functions (used by proxy)
-game_export_t	*pme;// export functions returned to server
-game_import_t   gi;  // import functions to be used by game/next proxy module
-game_export_t   ge;  // export functions returned from game/next proxy module
+game_export_t*	pme;// export functions returned to server
+game_import_t	gi;  // import functions to be used by game/next proxy module
+game_export_t	ge;  // export functions returned from game/next proxy module
 
-cvar_t  *pm_version;
-cvar_t  *basedir;
-cvar_t  *game;
-cvar_t  *proxy;
-cvar_t	*nextproxy;
+cvar_t* pm_version;
+cvar_t* basedir;
+cvar_t* game;
+cvar_t* proxy;
+cvar_t* nextproxy;
 
-void ShutdownProxyMod (void)
+void ShutdownProxyMod(void)
 {
-    ge.Shutdown(); // shutdown game DLL
-    FreeLibrary(hGameDLL);
-    pmi.dprintf ("=== Shutdown (ProxyMod) ===\n");
+	ge.Shutdown(); // shutdown game DLL
+	FreeLibrary(hGameDLL);
+	pmi.dprintf("=== Shutdown (ProxyMod) ===\n");
 }
 
 
-void InitProxyMod (void)
+void InitProxyMod(void)
 {
-	pmi.dprintf ("==== Init (ProxyMod) ====\n");
-    ge.Init();
+	pmi.dprintf("==== Init (ProxyMod) ====\n");
+	ge.Init();
 }
 
 
-void LoadGameModule(char *game_basedir, char *game_dir)
+void LoadGameModule(char* game_basedir, char* game_dir)
 {
 	char GameLibPath[MAX_OSPATH];
 
-    if (game_dir == NULL || strlen(game_dir)==0 || strchr(game_dir,'\\')!=NULL || strchr(game_dir,'/')!=NULL || strstr(game_dir,"..")!=NULL) {
-		pmi.cvar_forceset("nextproxy","");
-        sprintf(GameLibPath,"%s/baseq2/" GAME_MODULE, basedir->string);
-        pmi.dprintf("...loading default game module \"%s\": ",GameLibPath);
-    } else
-        if (strlen(game_basedir)!=0) {
-            sprintf(GameLibPath,"%s/%s/%s/" GAME_MODULE, basedir->string, game_basedir, game_dir);
-            pmi.dprintf("...loading proxy module \"%s\": ",GameLibPath);
-        } else {
-            sprintf(GameLibPath,"%s/%s/" GAME_MODULE, basedir->string, game_dir);
-            pmi.dprintf("...loading game module \"%s\": ",GameLibPath);
-        }
-    hGameDLL = LoadLibrary(GameLibPath);
-    if (hGameDLL != NULL)
-        pmi.dprintf("ok\n");
-    else {
-        pmi.dprintf("failed\n");
-        if (strlen(game_basedir)!=0) { // proxy chain aborted - load game
-			pmi.cvar_forceset("nextproxy","");
-            LoadGameModule("", game->string);
-            return;
-        }
-        if (strlen(game_dir)==0)    // failed to load default game module
-            return;
-        LoadGameModule("", ""); // attempt to load default module
-    }
-    return;
+	if (game_dir == NULL || strlen(game_dir) == 0 || strchr(game_dir, '\\') != NULL || strchr(game_dir, '/') != NULL || strstr(game_dir, "..") != NULL) {
+		pmi.cvar_forceset("nextproxy", "");
+		sprintf(GameLibPath, "%s/baseq2/" GAME_MODULE, basedir->string);
+		pmi.dprintf("...loading default game module \"%s\": ", GameLibPath);
+	}
+	else
+		if (strlen(game_basedir) != 0) {
+			sprintf(GameLibPath, "%s/%s/%s/" GAME_MODULE, basedir->string, game_basedir, game_dir);
+			pmi.dprintf("...loading proxy module \"%s\": ", GameLibPath);
+		}
+		else {
+			sprintf(GameLibPath, "%s/%s/" GAME_MODULE, basedir->string, game_dir);
+			pmi.dprintf("...loading game module \"%s\": ", GameLibPath);
+		}
+	hGameDLL = LoadLibrary(GameLibPath);
+	if (hGameDLL != NULL)
+		pmi.dprintf("ok\n");
+	else {
+		pmi.dprintf("failed\n");
+		if (strlen(game_basedir) != 0) { // proxy chain aborted - load game
+			pmi.cvar_forceset("nextproxy", "");
+			LoadGameModule("", game->string);
+			return;
+		}
+		if (strlen(game_dir) == 0)    // failed to load default game module
+			return;
+		LoadGameModule("", ""); // attempt to load default module
+	}
+	return;
 }
 
 
 
-typedef game_export_t *(*GetGameAPI_t) (game_import_t *);
+typedef game_export_t* (*GetGameAPI_t) (game_import_t*);
 
-game_export_t *GetGameAPI (game_import_t *gimport)
+game_export_t* GetGameAPI(game_import_t* gimport)
 {
 	char CurrentProxy[MAX_OSPATH];
 	char NextProxy[MAX_OSPATH];
-	char *LoopProxy;
-    char *colon;
+	char* LoopProxy;
+	char* colon;
 	int nextcolon;
-    GetGameAPI_t GetGameAPI_f;
+	GetGameAPI_t GetGameAPI_f;
 
-    memcpy(&pmi,gimport,sizeof(game_import_t)); 
+	memcpy(&pmi, gimport, sizeof(game_import_t));
 
-    pmi.dprintf("\nProxyMod v" PROXYMODVERNUM " " PROXYMODVERSTATUS "\nCopyright (c) 1998, by Avi Rozen (Zung!)\ne-mail: avrozen@ibm.net\n\n");
+	pmi.dprintf("\nProxyMod v" PROXYMODVERNUM " " PROXYMODVERSTATUS "\nCopyright (c) 1998, by Avi Rozen (Zung!)\ne-mail: avrozen@ibm.net\n\n");
 
-    pm_version = pmi.cvar("pm_version", PROXYMODVERSION, CVAR_NOSET);
-    basedir = pmi.cvar("basedir", ".", CVAR_NOSET);
-    game    = pmi.cvar("game", "", CVAR_SERVERINFO | CVAR_LATCH);
-    proxy   = pmi.cvar("proxy", "", CVAR_SERVERINFO | CVAR_LATCH);
+	pm_version = pmi.cvar("pm_version", PROXYMODVERSION, CVAR_NOSET);
+	basedir = pmi.cvar("basedir", ".", CVAR_NOSET);
+	game = pmi.cvar("game", "", CVAR_SERVERINFO | CVAR_LATCH);
+	proxy = pmi.cvar("proxy", "", CVAR_SERVERINFO | CVAR_LATCH);
 	nextproxy = pmi.cvar("nextproxy", "", CVAR_NOSET);
 
-	pmi.cvar_forceset("pm_version",PROXYMODVERSION);
+	pmi.cvar_forceset("pm_version", PROXYMODVERSION);
 
-    CurrentProxy[0]='\0';
-    NextProxy[0]='\0';
+	CurrentProxy[0] = '\0';
+	NextProxy[0] = '\0';
 
-	if ((proxy->string[0]!='\0') && (nextproxy->string[0]=='\0'))
-        pmi.cvar_forceset("nextproxy",proxy->string);
-    if (nextproxy->string[0]!='\0' && strcmp(nextproxy->string,":")!=0) {
-        if ((colon=strchr(nextproxy->string, ':'))==NULL)
-            strcpy(CurrentProxy, nextproxy->string);
-        else {
-            strncpy(CurrentProxy,nextproxy->string,colon - nextproxy->string);
-            CurrentProxy[min(MAX_OSPATH-1,colon - nextproxy->string)]='\0';
-            // prevent infinite loops
-            if ((LoopProxy=strstr(colon+1,CurrentProxy))!=NULL) {
+	if ((proxy->string[0] != '\0') && (nextproxy->string[0] == '\0'))
+		pmi.cvar_forceset("nextproxy", proxy->string);
+	if (nextproxy->string[0] != '\0' && strcmp(nextproxy->string, ":") != 0) {
+		if ((colon = strchr(nextproxy->string, ':')) == NULL)
+			strcpy(CurrentProxy, nextproxy->string);
+		else {
+			strncpy(CurrentProxy, nextproxy->string, colon - nextproxy->string);
+			CurrentProxy[min(MAX_OSPATH - 1, colon - nextproxy->string)] = '\0';
+			// prevent infinite loops
+			if ((LoopProxy = strstr(colon + 1, CurrentProxy)) != NULL) {
 				// weird stuff: win95 considers "folder  " and "folder" to be the same
 				// while "  folder" and "folder" are different (should be different in
 				// both cases!)
-				nextcolon=strlen(CurrentProxy);
+				nextcolon = strlen(CurrentProxy);
 				while (LoopProxy[nextcolon] && isspace(LoopProxy[nextcolon]))
 					nextcolon++;
-				if ((LoopProxy[nextcolon]==':' ||
-                     LoopProxy[nextcolon]=='\0') &&
-				    (NextProxy[LoopProxy-NextProxy-1]==':')) {
+				if ((LoopProxy[nextcolon] == ':' ||
+					LoopProxy[nextcolon] == '\0') &&
+					(NextProxy[LoopProxy - NextProxy - 1] == ':')) {
 					pmi.dprintf("Warning: found a loop in proxy chain!\n");
 					colon--; // hack: this will cause next proxy load to fail
 				}
-            } 
-            strcpy(NextProxy,colon+1);
-        }
-		if (NextProxy[0]!='\0')
-			pmi.cvar_forceset("nextproxy",NextProxy);
+			}
+			strcpy(NextProxy, colon + 1);
+		}
+		if (NextProxy[0] != '\0')
+			pmi.cvar_forceset("nextproxy", NextProxy);
 		else
-			pmi.cvar_forceset("nextproxy",":");
-    }
-    if ((CurrentProxy[0]!='\0') && strcmp(CurrentProxy,".."))
-        LoadGameModule("proxy",CurrentProxy);
-    else {
-		pmi.cvar_forceset("nextproxy","");
-        LoadGameModule("",game->string);
+			pmi.cvar_forceset("nextproxy", ":");
+	}
+	if ((CurrentProxy[0] != '\0') && strcmp(CurrentProxy, ".."))
+		LoadGameModule("proxy", CurrentProxy);
+	else {
+		pmi.cvar_forceset("nextproxy", "");
+		LoadGameModule("", game->string);
 	}
 
 
-    if (hGameDLL == NULL) {
-        pmi.error("game module not found");
-        return NULL;
-    }
+	if (hGameDLL == NULL) {
+		pmi.error("game module not found");
+		return NULL;
+	}
 
-    GetGameAPI_f = (GetGameAPI_t) GetProcAddress(hGameDLL,"GetGameAPI");
+	GetGameAPI_f = (GetGameAPI_t)GetProcAddress(hGameDLL, "GetGameAPI");
 
-    if (GetGameAPI_f == NULL) {
-        pmi.error("can't get game API");
-        return NULL;
-    }
+	if (GetGameAPI_f == NULL) {
+		pmi.error("can't get game API");
+		return NULL;
+	}
 
-    memcpy(&gi,gimport,sizeof(game_import_t));
-    // to capture API calls from game module change gi to point
-    // to local functions (i.e. gi.dprintf = proxy_dprintf;)
+	memcpy(&gi, gimport, sizeof(game_import_t));
+	// to capture API calls from game module change gi to point
+	// to local functions (i.e. gi.dprintf = proxy_dprintf;)
 
-    pme = GetGameAPI_f(&gi);
+	pme = GetGameAPI_f(&gi);
 
-	if (strcmp(nextproxy->string,":")==0)
-		pmi.cvar_forceset("nextproxy","");
+	if (strcmp(nextproxy->string, ":") == 0)
+		pmi.cvar_forceset("nextproxy", "");
 
-    memcpy(&ge,pme,sizeof(game_export_t));
-    // to capture API calls from server change pme to point
-    // to local functions (i.e. pme->ClientConnect = proxy_ClientConnect;)
+	memcpy(&ge, pme, sizeof(game_export_t));
+	// to capture API calls from server change pme to point
+	// to local functions (i.e. pme->ClientConnect = proxy_ClientConnect;)
 
-    pme->Init = InitProxyMod;
-    pme->Shutdown = ShutdownProxyMod;
+	pme->Init = InitProxyMod;
+	pme->Shutdown = ShutdownProxyMod;
 
-    return pme;
+	return pme;
 }
--- proxymod.h
+++ proxymod.h
@@ -30,7 +30,7 @@ extern HMODULE hGameDLL;
 #define	MAX_CLIENTS			256
 
 typedef unsigned char 		byte;
-typedef enum {false, true}	qboolean;
+typedef enum { false, true }	qboolean;
 
 #ifndef NULL
 #define NULL ((void *)0)
@@ -46,13 +46,13 @@ typedef enum {false, true}	qboolean;
 
 typedef struct cvar_s
 {
-	char		*name;
-	char		*string;
-	char		*latched_string;
+	char*		name;
+	char*		string;
+	char*		latched_string;
 	int			flags;
 	qboolean	modified;
 	float		value;
-	struct cvar_s *next;
+	struct cvar_s* next;
 } cvar_t;
 
 
@@ -88,13 +88,13 @@ typedef struct
 	float		fraction;
 	vec3_t		endpos;
 	cplane_t	plane;
-	csurface_t	*surface;
+	csurface_t* surface;
 	int			contents;
-	struct edict_s	*ent;
+	struct edict_s* ent;
 } trace_t;
 
 
-typedef enum 
+typedef enum
 {
 	PM_NORMAL,
 	PM_SPECTATOR,
@@ -164,7 +164,7 @@ typedef struct entity_state_s
 
 typedef struct link_s
 {
-	struct link_s	*prev, *next;
+	struct link_s* prev, * next;
 } link_t;
 
 typedef enum
@@ -180,7 +180,7 @@ typedef enum
 struct edict_s
 {
 	entity_state_t	s;
-	struct gclient_s	*client;
+	struct gclient_s* client;
 	qboolean	inuse;
 	int			linkcount;
 	link_t		area;
@@ -193,7 +193,7 @@ struct edict_s
 	vec3_t		absmin, absmax, size;
 	solid_t		solid;
 	int			clipmask;
-	edict_t		*owner;
+	edict_t* owner;
 };
 
 #define	BUTTON_ATTACK		1
@@ -206,8 +206,8 @@ typedef struct usercmd_s
 	byte	buttons;
 	short	angles[3];
 	short	forwardmove, sidemove, upmove;
-	byte	impulse;		
-	byte	lightlevel;		
+	byte	impulse;
+	byte	lightlevel;
 } usercmd_t;
 
 
@@ -219,14 +219,14 @@ typedef struct
 	usercmd_t		cmd;
 	qboolean		snapinitial;
 	int			numtouch;
-	struct edict_s	*touchents[MAXTOUCH];
+	struct edict_s* touchents[MAXTOUCH];
 	vec3_t		viewangles;
 	float		viewheight;
 	vec3_t		mins, maxs;
-	struct edict_s	*groundentity;
+	struct edict_s* groundentity;
 	int			watertype;
 	int			waterlevel;
-	trace_t		(*trace) (vec3_t start, vec3_t mins, vec3_t maxs, vec3_t end);
+	trace_t(*trace) (vec3_t start, vec3_t mins, vec3_t maxs, vec3_t end);
 	int			(*pointcontents) (vec3_t point);
 } pmove_t;
 
@@ -248,60 +248,60 @@ typedef struct
 
 typedef enum
 {
-    MULTICAST_ALL,
-    MULTICAST_PHS,
-    MULTICAST_PVS,
-    MULTICAST_ALL_R,
-    MULTICAST_PHS_R,
-    MULTICAST_PVS_R
+	MULTICAST_ALL,
+	MULTICAST_PHS,
+	MULTICAST_PVS,
+	MULTICAST_ALL_R,
+	MULTICAST_PHS_R,
+	MULTICAST_PVS_R
 } multicast_t;
 
 
 typedef struct
 {
-	void	(*bprintf) (int printlevel, char *fmt, ...);
-	void	(*dprintf) (char *fmt, ...);
-	void	(*cprintf) (edict_t *ent, int printlevel, char *fmt, ...);
-	void	(*centerprintf) (edict_t *ent, char *fmt, ...);
-	void	(*sound) (edict_t *ent, int channel, int soundindex, float volume, float attenuation, float timeofs);
-	void	(*positioned_sound) (vec3_t origin, edict_t *ent, int channel, int soundinedex, float volume, float attenuation, float timeofs);
-	void	(*configstring) (int num, char *string);
-	void	(*error) (char *fmt, ...);
-	int		(*modelindex) (char *name);
-	int		(*soundindex) (char *name);
-	int		(*imageindex) (char *name);
-	void	(*setmodel) (edict_t *ent, char *name);
-	trace_t	(*trace) (vec3_t start, vec3_t mins, vec3_t maxs, vec3_t end, edict_t *passent, int contentmask);
+	void	(*bprintf) (int printlevel, char* fmt, ...);
+	void	(*dprintf) (char* fmt, ...);
+	void	(*cprintf) (edict_t* ent, int printlevel, char* fmt, ...);
+	void	(*centerprintf) (edict_t* ent, char* fmt, ...);
+	void	(*sound) (edict_t* ent, int channel, int soundindex, float volume, float attenuation, float timeofs);
+	void	(*positioned_sound) (vec3_t origin, edict_t* ent, int channel, int soundinedex, float volume, float attenuation, float timeofs);
+	void	(*configstring) (int num, char* string);
+	void	(*error) (char* fmt, ...);
+	int		(*modelindex) (char* name);
+	int		(*soundindex) (char* name);
+	int		(*imageindex) (char* name);
+	void	(*setmodel) (edict_t* ent, char* name);
+	trace_t(*trace) (vec3_t start, vec3_t mins, vec3_t maxs, vec3_t end, edict_t* passent, int contentmask);
 	int		(*pointcontents) (vec3_t point);
-	qboolean	(*inPVS) (vec3_t p1, vec3_t p2);
-	qboolean	(*inPHS) (vec3_t p1, vec3_t p2);
+	qboolean(*inPVS) (vec3_t p1, vec3_t p2);
+	qboolean(*inPHS) (vec3_t p1, vec3_t p2);
 	void		(*SetAreaPortalState) (int portalnum, qboolean open);
-	qboolean	(*AreasConnected) (int area1, int area2);
-	void	(*linkentity) (edict_t *ent);
-	void	(*unlinkentity) (edict_t *ent);
-	int		(*BoxEdicts) (vec3_t mins, vec3_t maxs, edict_t **list,	int maxcount, int areatype);
-	void	(*Pmove) (pmove_t *pmove);
+	qboolean(*AreasConnected) (int area1, int area2);
+	void	(*linkentity) (edict_t* ent);
+	void	(*unlinkentity) (edict_t* ent);
+	int		(*BoxEdicts) (vec3_t mins, vec3_t maxs, edict_t** list, int maxcount, int areatype);
+	void	(*Pmove) (pmove_t* pmove);
 	void	(*multicast) (vec3_t origin, multicast_t to);
-	void	(*unicast) (edict_t *ent, qboolean reliable);
+	void	(*unicast) (edict_t* ent, qboolean reliable);
 	void	(*WriteChar) (int c);
 	void	(*WriteByte) (int c);
 	void	(*WriteShort) (int c);
 	void	(*WriteLong) (int c);
 	void	(*WriteFloat) (float f);
-	void	(*WriteString) (char *s);
+	void	(*WriteString) (char* s);
 	void	(*WritePosition) (vec3_t pos);
 	void	(*WriteDir) (vec3_t pos);
 	void	(*WriteAngle) (float f);
-	void	*(*TagMalloc) (int size, int tag);
-	void	(*TagFree) (void *block);
+	void* (*TagMalloc) (int size, int tag);
+	void	(*TagFree) (void* block);
 	void	(*FreeTags) (int tag);
-	cvar_t	*(*cvar) (char *var_name, char *value, int flags);
-	cvar_t	*(*cvar_set) (char *var_name, char *value);
-	cvar_t	*(*cvar_forceset) (char *var_name, char *value);
+	cvar_t* (*cvar) (char* var_name, char* value, int flags);
+	cvar_t* (*cvar_set) (char* var_name, char* value);
+	cvar_t* (*cvar_forceset) (char* var_name, char* value);
 	int		(*argc) (void);
-	char	*(*argv) (int n);
-	char	*(*args) (void);
-	void	(*AddCommandString) (char *text);
+	char* (*argv) (int n);
+	char* (*args) (void);
+	void	(*AddCommandString) (char* text);
 	void	(*DebugGraph) (float value, int color);
 } game_import_t;
 
@@ -311,20 +311,20 @@ typedef struct
 	int			apiversion;
 	void		(*Init) (void);
 	void		(*Shutdown) (void);
-	void		(*SpawnEntities) (char *mapname, char *entstring, char *spawnpoint);
-	void		(*WriteGame) (char *filename, qboolean autosave);
-	void		(*ReadGame) (char *filename);
-	void		(*WriteLevel) (char *filename);
-	void		(*ReadLevel) (char *filename);
-	qboolean	(*ClientConnect) (edict_t *ent, char *userinfo);
-	void		(*ClientBegin) (edict_t *ent);
-	void		(*ClientUserinfoChanged) (edict_t *ent, char *userinfo);
-	void		(*ClientDisconnect) (edict_t *ent);
-	void		(*ClientCommand) (edict_t *ent);
-	void		(*ClientThink) (edict_t *ent, usercmd_t *cmd);
+	void		(*SpawnEntities) (char* mapname, char* entstring, char* spawnpoint);
+	void		(*WriteGame) (char* filename, qboolean autosave);
+	void		(*ReadGame) (char* filename);
+	void		(*WriteLevel) (char* filename);
+	void		(*ReadLevel) (char* filename);
+	qboolean(*ClientConnect) (edict_t* ent, char* userinfo);
+	void		(*ClientBegin) (edict_t* ent);
+	void		(*ClientUserinfoChanged) (edict_t* ent, char* userinfo);
+	void		(*ClientDisconnect) (edict_t* ent);
+	void		(*ClientCommand) (edict_t* ent);
+	void		(*ClientThink) (edict_t* ent, usercmd_t* cmd);
 	void		(*RunFrame) (void);
 	void		(*ServerCommand) (void);
-	struct edict_s	*edicts;
+	struct edict_s* edicts;
 	int			edict_size;
 	int			num_edicts;
 	int			max_edicts;
@@ -336,9 +336,9 @@ typedef struct
 extern game_import_t gi;
 extern game_export_t ge;
 extern game_import_t pmi;
-extern game_export_t *pme;
+extern game_export_t* pme;
 
-extern cvar_t  *pm_version;
-extern cvar_t  *basedir;
-extern cvar_t  *game;
-extern cvar_t  *proxy;
+extern cvar_t* pm_version;
+extern cvar_t* basedir;
+extern cvar_t* game;
+extern cvar_t* proxy;
